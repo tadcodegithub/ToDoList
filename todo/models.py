8d5544a2cb63d12e86245f7229875312f05d3de6
Tadesse <tadess1127@gmail.com>
2024-01-04 08:55:03 +0300

    Todo list project is created with postgreSQL database

--- todo/models.py
+++ todo/models.py
@@ -7,4 +7,4 @@ class Task(models.Model):
     reg_date= models.DateField(default=timezone.now, null=True)
     objects = models.Manager()
     def __str__(self):
-        return self.title
+        return self.title  
