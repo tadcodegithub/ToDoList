45ac24ea5976214f5c9650a416b889ef182de732
Tadesse <tadess1127@gmail.com>
2024-01-04 08:49:14 +0300

    Todo list project is created with postgreSQL database

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False, blank=True,  null=True)
+    reg_date= models.DateField(default=timezone.now, null=True)
+    objects = models.Manager()
+    def __str__(self):
+        return self.title
